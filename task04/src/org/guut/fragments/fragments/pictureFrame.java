20292a68d8227b71a99340ff7bc0e4d6e54dd653
zalox <jorgen@guut.org>
2013-10-18 13:28:48 +0200

    init task04

---
+++ task04/src/org/guut/fragments/fragments/pictureFrame.java
@@ -0,0 +1,11 @@
+package org.guut.fragments.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class pictureFrame extends Fragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ task04/src/org/guut/fragments/run/main.java
@@ -0,0 +1,10 @@
+package org.guut.fragments.run;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
