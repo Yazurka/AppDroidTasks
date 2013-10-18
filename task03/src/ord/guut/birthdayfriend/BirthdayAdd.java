5321445d2349a323f418d1b47896f69a41562d2a
Zalox <jorgen@kultar.no>
2013-10-18 13:35:35 +0200

    cmont

---
+++ task03/src/ord/guut/birthdayfriend/BirthdayAdd.java
@@ -0,0 +1,11 @@
+package ord.guut.birthdayfriend;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class BirthdayAdd extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ task03/src/ord/guut/birthdayfriend/BirthdayList.java
@@ -0,0 +1,17 @@
+package ord.guut.birthdayfriend;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class BirthdayList extends ListActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.birthdaylist);
+    }
+
+    public void showBirthDay(View view) {
+
+    }
+}
