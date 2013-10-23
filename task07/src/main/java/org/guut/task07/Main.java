6637676255979b4fc6d56e21b00bcf84edd7af65
Zalox <jorgen@kultar.no>
2013-10-23 18:47:34 +0200

    Add root for task06 and task07

---
+++ task07/src/main/java/org/guut/task07/Main.java
@@ -0,0 +1,17 @@
+package org.guut.task07;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Class Main
+ *
+ * @author Jørgen Lien Sellæg
+ */
+public class Main extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
