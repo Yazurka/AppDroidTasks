59d7f8b748ea94f096b82d79fd61d02f45f85f4e
Zalox <jorgen@kultar.no>
2013-10-23 22:05:20 +0200

    Task 06 start view made

---
+++ task06/src/main/java/org/guut/task06/fragments/PlayBoard.java
@@ -0,0 +1,21 @@
+package org.guut.task06.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.guut.task06.R;
+
+/**
+ * Class ${CLASS_NAME}
+ *
+ * @author Jørgen Lien Sellæg
+ */
+public class PlayBoard extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.player_form_fragment_layout, container, false); //TODO change the layout
+    }
+}
---
+++ task06/src/main/java/org/guut/task06/httpClasses/Communicator.java
@@ -0,0 +1,9 @@
+package org.guut.task06.httpClasses;
+
+/**
+ * Class ${CLASS_NAME}
+ *
+ * @author Jørgen Lien Sellæg
+ */
+public class Communicator {
+}
